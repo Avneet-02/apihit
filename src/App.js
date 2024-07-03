ac1d96c27278be888d71feeb291d2a90fe1a52b5
avneet <kauravi1212@gmail.com>
2024-07-03 16:44:55 +0530

    call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [mydata, setMyData] = useState([]);
+  const [isError, setIsError] = useState("");
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => setMyData(res.data))
+      .catch((error) => setIsError(error.message));
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Axios Tutorial</h1>
+      {isError && <h2>{isError}</h2>}
+      {selectedPost ? (
+        <div className="detail-view">
+          <h2>{selectedPost.title.toUpperCase()}</h2>
+          <p>{selectedPost.body}</p>
+          <button onClick={() => setSelectedPost(null)}>Back</button>
+        </div>
+      ) : (
+        <div className="grid">
+          {mydata.map((post) => {
+            const { id, title, body } = post;
+            return (
+              <div className="card" key={id} onClick={() => setSelectedPost(post)}>
+                <h2>{title.slice(0, 15).toUpperCase()}</h2>
+                <p>{body.slice(0, 100)}</p>
+              </div>  
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 }
 
